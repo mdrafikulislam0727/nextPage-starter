f8d556d12ed34b931f01998ba2e68fe9bcce2ed6
mdrafikulislam <mdrafikulislam0727@gmail.com>
2023-04-08 02:57:34 -0700

    all complite project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className='App'>
       {/* header */}
       <Header></Header>
+      <div className="min-h-[calc(100vh-136px)]">
       <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
       {/* footer */}
     </div>
   )
